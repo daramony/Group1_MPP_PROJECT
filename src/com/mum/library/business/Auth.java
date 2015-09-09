5036f96a56f1e225d9a258591200fc9e98e849f9
ChengMa <984724@cs_labv47a-104.cs.mum.edu>
2015-09-09 10:46:49 -0500

    merge branch

--- src/com/mum/library/business/Auth.java
+++
@@ -1,7 +0,0 @@
-package com.mum.library.business;
-
-import java.io.Serializable;
-
-public enum Auth implements Serializable {
-	LIBRARIAN, ADMIN, BOTH;
-}
--- src/com/mum/library/controller/AddBookController.java
+++
@@ -1,5 +0,0 @@
-package com.mum.library.controller;
-
-public class AddBookController {
-
-}
