26208f4cfcc02455c08a43deaebbff85dc86577f
Pratheeban <pratheebanuj@gmail.com>
2015-09-10 15:37:14 -0400

    Add Book and Person

--- src/com/mum/library/business/Book.java
+++ src/com/mum/library/business/Book.java
@@ -4,101 +4,61 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Book extends Publication implements Serializable {
 	private String isbn;
+	private int maxLendDaysAllowed;
+	private int noOfCopies;
 	private boolean available;
 	private List<Author> authors;
-	private List<LendableCopy> copies = new ArrayList<LendableCopy>();
-	private String copiesNumString;
-	
-	
-	public String getCopiesNumString() {
-		return getCopiesString();
-	}
+	private List<LendableCopy> copies;
 
-	public void setCopiesNumString(String copiesNumString) {
-		this.copiesNumString = copiesNumString;
-	}
-
-	public List<LendableCopy> getCopies() {
-		return copies;
-	}
-
-	public void setCopies(List<LendableCopy> copies) {
-		this.copies = copies;
-	}
-	
-	public String getCopiesString() {
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append("");
-		sb.append(this.copies.size());
-		String strI = sb.toString();
-		
-		return strI;
-	}
-
-	public Book(String isbn, String title,List<Author> authors,int copyNum) {
+	public Book(String isbn, String title, int maxLendDaysAllowed, int noOfcopies, List<Author> authors) {
 		super(title);
-		this.isbn=isbn;
-		this.authors=authors;
-		
-		for(int i=0; i<copyNum; i++) {
+		this.isbn = isbn;
+		this.maxLendDaysAllowed = maxLendDaysAllowed;
+		this.authors = authors;
+		this.noOfCopies = noOfcopies;
+		copies = new ArrayList<>();
+		for (int i = 0; i < noOfCopies; i++) {
 			addCopy();
 		}
-		
+
 	}
-	
 	public void addCopy() {
 		LendableCopy copy = new LendableCopy(this, this.copies.size()+1);
 		copies.add(copy);
 	}
 
+	public void isAvailable(boolean b) {
+		available = b;
+	}
 
 	public String getIsbn() {
 		return isbn;
 	}
 
-	
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
 
-
-
-	public boolean isAvailable() {
-		return available;
+	public int getMaxLendDaysAllowed() {
+		return maxLendDaysAllowed;
 	}
 
-
+	public void setMaxLendDaysAllowed(int maxLendDaysAllowed) {
+		this.maxLendDaysAllowed = maxLendDaysAllowed;
+	}
 
 	public void setAvailable(boolean available) {
 		this.available = available;
 	}
 
-
-
 	public List<Author> getAuthors() {
 		return authors;
 	}
-
-
 
 	public void setAuthors(List<Author> authors) {
 		this.authors = authors;
 	}
 
-
-
-		public void isAvailable(boolean b) {
-			available = b;
-		}
-		
-		@Override
-		public String toString() {
-			
-			return "Title:"+this.getTitle()+", ISBN:"+this.getIsbn();
-		}
-		
 }
--- src/com/mum/library/business/Person.java
+++ src/com/mum/library/business/Person.java
@@ -6,36 +6,45 @@ public class Person implements Serializable {
 	private static final long serialVersionUID = 3665880920647848288L;
 	private String firstName;
 	private String lastName;
-	private String telephone;
+	private String phoneNo;
 	private Address address;
-	public Person(String f, String l, String t, Address a) {
-		firstName = f;
-		lastName = l;
-		telephone = t;
-		address = a;
+
+	public Person(String firstName, String lastName, String phoneNo, Address address) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNo = phoneNo;
+		this.address = address;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
-	public String getTelephone() {
-		return telephone;
-	}
+
 	public Address getAddress() {
 		return address;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
+
 	public void setAddress(Address address) {
 		this.address = address;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
 	}
 }
