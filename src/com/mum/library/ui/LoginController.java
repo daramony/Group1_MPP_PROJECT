fd0f705848f6ccbcecfc7d5119d3212b73d30a86
ChengMa <mcking007@gmail.com>
2015-09-09 21:03:50 -0500

    added login logic

--- src/com/mum/library/ui/LoginController.java
+++ src/com/mum/library/ui/LoginController.java
@@ -4,14 +4,20 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.mum.library.business.User;
+import com.mum.library.dataaccess.DataAccess;
+import com.mum.library.dataaccess.DataAccessFacade;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -38,33 +44,43 @@ public class LoginController {
 
     @FXML
     void login(ActionEvent event) {
-    
-    	try {
-//    		FXMLLoader loader = new FXMLLoader(getClass().getResource(
-//				"MainFrame1.fxml")); 
-//    		
-//			Parent root = loader.load();
-//   	
-    	Stage stage = new Stage();
-//		stage.setTitle("Library System");
-//		stage.setScene(new Scene(root));
-//		stage.show();
-    	// Close login window
-    				Window window = header.getScene().getWindow();
-    				if (window instanceof Stage) {
-    					((Stage) window).close();
-    				}
-    		Parent root = FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
-    		stage.setTitle("Library System");
-    		Scene scene = new Scene(root);
-    		stage.setScene(scene);
-    		stage.show();
-    		
-    		
-    	} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+    	String username=TxtUsername.getText();
+    	String password=TxtPassword.getText();
+    	
+    	DataAccess da=new DataAccessFacade();
+    	
+    	if (da.readUserMap().get(username)!=null&&da.readUserMap().get(username).getPassword().equals(password)) {
+    		try {
+    			
+    			Alert alert = new Alert(AlertType.INFORMATION);
+    			alert.setTitle("Library System");
+    			alert.setHeaderText(null);
+    			alert.setContentText("WELCOME "+username+"!");
+    			alert.showAndWait();
+    			
+    	    	Stage stage = new Stage();
+    	    				Window window = header.getScene().getWindow();
+    	    				if (window instanceof Stage) {
+    	    					((Stage) window).close();
+    	    				}
+    	    		Parent root = FXMLLoader.load(getClass().getResource("MainFrame.fxml"));
+    	    		stage.setTitle("Library System");
+    	    		Scene scene = new Scene(root);
+    	    		stage.setScene(scene);
+    	    		stage.show();    		
+    	    		
+    	    	} catch (IOException e) {
+    				e.printStackTrace();
+    			}
+		}else {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("Library System");
+			alert.setHeaderText(null);
+			alert.setContentText("You input the wrong username or password,please try again!");
+			alert.showAndWait();
 		}
+    
+    
     }
 
     @FXML
