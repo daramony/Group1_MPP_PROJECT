b8faffe636952433a0c35fc06a3d86d017edfea5
Pratheeban <pratheebanuj@gmail.com>
2015-09-09 23:05:15 -0400

    Add Book, Author and Person

---
+++ src/com/mum/library/model/Book.java
@@ -0,0 +1,52 @@
+package com.mum.library.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Book extends Publication implements Serializable {
+	private String isbn;
+	private int maxLendDaysAllowed;
+	private boolean available;
+	private List<Author> authors;
+
+	public Book(String isbn, String title, int maxLendDaysAllowed, List<Author> authors) {
+		super(title);
+		this.isbn = isbn;
+		this.maxLendDaysAllowed = maxLendDaysAllowed;
+		this.authors = authors;
+	}
+
+	public void isAvailable(boolean b) {
+		available = b;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getMaxLendDaysAllowed() {
+		return maxLendDaysAllowed;
+	}
+
+	public void setMaxLendDaysAllowed(int maxLendDaysAllowed) {
+		this.maxLendDaysAllowed = maxLendDaysAllowed;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+
+	public List<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<Author> authors) {
+		this.authors = authors;
+	}
+	
+
+}
---
+++ src/com/mum/library/model/Person.java
@@ -0,0 +1,50 @@
+package com.mum.library.model;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+	private static final long serialVersionUID = 3665880920647848288L;
+	private String firstName;
+	private String lastName;
+	private String phoneNo;
+	private Address address;
+
+	public Person(String firstName, String lastName, String phoneNo, Address address) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNo = phoneNo;
+		this.address = address;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+}
